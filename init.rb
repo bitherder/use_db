29748b312af76024f6fac5e51194f9f857c6ff55
Stefan Kaes <skaes@railsexpress.de>
2010-12-27 11:34:36 +0100

    only load test related code when running inside the test environment

--- init.rb
+++ init.rb
@@ -1,9 +1,11 @@
 require "use_db_plugin"
-require "use_db_test"
-require 'active_record/fixtures'
-require "override_fixtures"
+if RAILS_ENV=="test"
+  require "use_db_test"
+  require 'active_record/fixtures'
+  require "override_fixtures"
+  require 'override_test_callbacks'
+end
 require 'active_record/migration'
-require 'override_test_callbacks'
 require "migration"
 
 ActiveRecord::Base.extend(UseDbPlugin)
